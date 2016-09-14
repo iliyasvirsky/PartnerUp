24c6ff0edbf171379afed9797575f45347323476
iliya svirsky <iliyasvirsky@gmail.com>
2016-09-14 11:44:22 -0500

    fixed merge

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 var url = 'postgres://hzhzhysgpursgg:Mkih7oW9Ek6dGdTSmyuVgxw3kr@ec2-54-163-245-32.compute-1.amazonaws.com:5432/dd9bnae9j8734d'
 
+
 module.exports = { 
     client: 'postgresql',
     connection: url,
