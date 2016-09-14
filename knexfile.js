1cfe1d9a5104cb4e9856057f403c7a1160a2631c
Ryan Walter <r.walter215@gmail.com>
2016-09-14 16:07:26 -0500

    small change knex

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,9 @@
 var url = 'postgres://hzhzhysgpursgg:Mkih7oW9Ek6dGdTSmyuVgxw3kr@ec2-54-163-245-32.compute-1.amazonaws.com:5432/dd9bnae9j8734d'
 
+
 module.exports = { 
     client: 'postgresql',
+<<<<<<< f28ad2cc021ceff8ec8bad0ff23e41946d582fad
     connection: url
       // host: 'ec2-54-163-245-32.compute-1.amazonaws.com',
       // database: 'dd9bnae9j8734d',
