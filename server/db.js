6153077698ea43ea7612d8c7f213270616a99d25
Elliot Cheung <elliotccheung@gmail.com>
2016-09-22 16:18:05 -0500

    Edited the error message for unauthorized

--- server/db.js
+++ server/db.js
@@ -20,7 +20,7 @@ knex.authenticate = (sessionUid) => {
   if(sessionUid || process.env.TEST_AUTH) {
     return Promise.resolve();
   } else {
-    return Promise.reject("401 Unauthorized, Please make sure you have acess to that page");
+    return Promise.reject("401 Unauthorized, please make sure you are logged in");
   }
 }
 
